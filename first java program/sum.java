20201b4b0e2adb001aaacf148cde4d3968b90b0c
Het patel <patelhet300324@gmail.com>
2026-06-05 01:29:31 +0530

    added java code

---
+++ first java program/sum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter the first number");
+        int num1 = input.nextInt();
+        System.out.println("enter the second number");
+        int num2 = input.nextInt();
+        int sum = num1 + num2;
+        System.out.println("the sum of " + num1 + " and " + num2 + " is " + sum);
+    }
+
+
+    
+}
