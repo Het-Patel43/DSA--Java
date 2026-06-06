dd4dba4a61df31cea33bc55a0e434837a04ef30c
Het patel <patelhet300324@gmail.com>
2026-06-06 15:45:45 +0530

    added to java

---
+++ Condition and Loops/loops.java
@@ -0,0 +1,92 @@
+
+import java .util.Scanner;
+public class loops {
+    public static void main(String[] args){
+
+// print number from 1 to 5;
+
+// syntax of for loop -->
+
+/*
+for(intialization ; condition ; icremenr/decrement){
+    code to be execcuted }
+
+*/
+
+// for(int i = 1 ; i<=5 ; i++){
+//     System.out.println(i);
+// }
+
+// print number from 0 to n
+
+// Scanner sc = new Scanner(System.in);
+// System.out.println("enter the number");
+
+// int n = sc.nextInt();
+// for(int i = 0 ; i<=n ; i++){
+//     System.out.println(i);
+// }
+
+
+
+// while loops -->
+
+/* syntax -->
+
+while(condition){
+ code to be executed;
+ }
+
+*/
+
+// print number from 1 to 5;
+
+// int num =1;
+// while(num<=5){
+//     System.out.println(num);
+//     num += 1;
+// }
+
+// When to use for loops and while loops -->
+
+  /*
+  
+  while loop is used when we dont know how many times the loop will run 
+
+
+  for loop is used when we no how many times the loop will run
+  
+  */
+
+
+
+
+
+  // do while loop
+
+  /*
+  syntax -->
+
+  do{
+code to be executed;  
+  
+}while(condition);
+  
+  
+  */
+
+// print number from 1 to 5;
+
+int n = 1;
+do{
+    System.out.println(n);
+    n +=1;
+}while(n<=5);
+
+
+// in do while loops the code will be executed atleast once even if the conditionis false
+
+    }
+
+    
+}
