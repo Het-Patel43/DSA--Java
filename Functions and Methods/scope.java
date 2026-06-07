b9b10480ed20233894719fcf94fd0d328b31aa32
Het patel <patelhet300324@gmail.com>
2026-06-08 01:45:22 +0530

    block scope example

--- Functions and Methods/scope.java
+++ Functions and Methods/scope.java
@@ -7,7 +7,10 @@ public class scope {
 int a  =10; 
 int b =20;
 
+
 {
+
+
    // int a  =70; // ERROR = duplicate varibale a 
 
    // int a is already intialzed outside the block so it cannot be intialzed again
@@ -15,7 +18,24 @@ int b =20;
    a = 40 ; // you can reasign the value to ref varibale 
 
    // value intialized in this block will be only in this block
+
+
+// scoping for loops ;
+
+for(int i = 0 ; i<10 ; i++){
+
+    System.out.println(i);
+    int num = 90 ; 
+       a  = 10;
 }
+
+// Anything intialzed outside can be used inside  but vice versa is not possible ;
+
+
+// Anything intailzed inside the box can be intialzed outside the box;
+
+}
+
 
 // same goes for string as well ;
 
