f885313c1bb9b33f06c98076b9ce268cb841f126
Het patel <patelhet300324@gmail.com>
2026-06-08 01:16:17 +0530

    call by value example

---
+++ Functions and Methods/changevalues.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class changevalues {
+    public static void main(String[]  args){
+
+
+// create a array ;
+
+int[] arr = {1 ,2 ,3 ,4,5, 6};
+change(arr);
+System.out.println(Arrays.toString(arr));
+
+    }
+
+static void change(int[] nums){
+    nums[0] = 69;
+// if you try to make change the object via this ref variable same object will also change ;
+
+}
+
+
+}
