8f068f5a49f50857c3838fac8b35a4dc4443d85b
Het patel <hetpatel2443@gmail.com>
2026-06-09 22:24:40 +0530

    passing arrays to function

---
+++ Arrays/passinginfunction.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+import java.util.Arrays;
+
+
+public class passinginfunction {
+
+    public static void main(String[] args){
+
+Scanner sc = new Scanner(System.in);
+
+int[] arr = new int[5];
+
+for(int i = 0 ; i< arr.length ; i++ ){
+
+    arr[i] = sc.nextInt();
+
+}
+
+System.out.println(Arrays.toString(arr));
+
+System.out.println(" After the change");
+
+change(arr);
+
+System.out.println(Arrays.toString(arr));
+
+
+
+sc.close();
+
+
+    }
+
+    static void change( int[] nums){
+
+        nums[3] = 99 ;
+    }
+    
+}
