636eb8b988f4c975b055a723d57de351884cd660
Het patel <patelhet300324@gmail.com>
2026-06-09 10:15:24 +0530

    function overloading

---
+++ Functions and Methods/overloading.java
@@ -0,0 +1,23 @@
+
+
+public class overloading {
+
+    public static void main(String[] args){
+
+fun(45);
+fun("het");
+
+    }
+
+    static void fun(int a){
+
+        System.out.println(a);
+
+    }
+
+    static void fun(String name){
+
+         System.out.println(name);
+    }
+    
+}
