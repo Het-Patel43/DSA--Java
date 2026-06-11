4375873426b12d67980b695f1febd4698a313b3b
Het patel <hetpatel2443@gmail.com>
2026-06-11 16:38:17 +0530

    Order-Agnostic Binary Search

--- Binary search/binarysearch.java
+++ Binary search/binarysearch.java
@@ -2,7 +2,7 @@ public class binarysearch{
 
     public static void main(String[] args){
 
-int [] arr = { 1 ,2 ,3,4 ,5, 6,7, 8, 9,10};
+int [] arr = {10 ,9,8,7,6,5,4,3,2,1};
 int ans = search(arr , 9);
 System.out.println(ans); 
 
@@ -12,7 +12,8 @@ System.out.println(ans);
     static int search(int[] arr , int target){
 
         int start = 0;
-        int  end = arr.length - 1;
+
+        int  end = arr.length - 1; 
 
 
         while(start <= end){
@@ -24,11 +25,12 @@ System.out.println(ans);
               int mid = start + (end - start ) / 2;
 
               if(target < arr[mid]) {
-                 end = mid- 1 ;
+                 start = mid + 1;
               }
 
               else if(target > arr[mid]){
-                start = mid + 1;
+                
+                end = mid- 1 ;
 
               }
 
