b8926087785da5bfbf4b4bab3e524bd5d5f24e71
Het patel <hetpatel2443@gmail.com>
2026-06-12 17:14:06 +0530

    Build array from permutation

---
+++ Arrays/questions/permutation.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class permutation{
+
+public static void main(String[] args){
+
+int[] nums = {1 ,2,3,4  };
+
+int[] ans = buildArray( nums);
+System.out.println(Arrays.toString(ans));
+
+}
+
+static int[] buildArray(int[] nums){
+
+    int[] ans = new int[nums.length];
+    for(int i = 0 ; i< nums.length ; i++){
+ans[i] = nums[nums[i]];
+
+    }
+
+    return ans ;
+}
+
+}
